c6fedb956ff180b19580ca7194fec5edaecabb10
Hinault <ngdonfack@gmail.com>
2020-11-29 10:21:06 -0500

    Ajoutr projet

---
+++ Game/Game.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace Game.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+        }
+
+        [Fact]
+        public void Test3()
+        {
+
+        }
+    }
+}
